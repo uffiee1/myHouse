ac1e80f0de4d506c3f6694743a6b808ab00d6fed
Ufuk Angay <u.angay@gmail.com>
2021-11-02 14:43:22 +0100

    test

--- myHouse/Startup.cs
+++ myHouse/Startup.cs
@@ -31,7 +31,7 @@ namespace myHouse
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        //  This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
