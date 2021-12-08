771f2b594ad03a917c83a06a59b2f1c77d018070
Ufuk Angay <u.angay@gmail.com>
2021-12-08 04:10:39 +0100

    Update UserController -WIP

--- myHouse/Controllers/UserController.cs
+++ myHouse/Controllers/UserController.cs
@@ -14,24 +14,24 @@ namespace myHouse.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [AllowAnonymous]
-        [HttpPost("login")]
-        public async Task<ActionResult<User>> Login(Login.Query query)
-        {
-            return await Mediator.Send(query);
-        }
+        //[AllowAnonymous]
+        //[HttpPost("login")]
+        //public async Task<ActionResult<User>> Login(Login.Query query)
+        //{
+        //    return await Mediator.Send(query);
+        //}
 
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<ActionResult<User>> Register(Register.Command command)
-        {
-            return await Mediator.Send(command);
-        }
+        //[AllowAnonymous]
+        //[HttpPost("register")]
+        //public async Task<ActionResult<User>> Register(Register.Command command)
+        //{
+        //    return await Mediator.Send(command);
+        //}
 
-        [HttpGet]
-        public async Task<ActionResult<User>> GetCurrentUser()
-        {
-            return await Mediator.Send(new CurrentUser.Query());
-        }
+        //[HttpGet]
+        //public async Task<ActionResult<User>> GetCurrentUser()
+        //{
+        //    return await Mediator.Send(new CurrentUser.Query());
+        //}
     }
 }
